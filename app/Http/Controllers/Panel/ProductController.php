c74f25323e2e1c2b6e818c57c0aaba28443f5425
amirrezaraadi13 <amirrezaraadi13@yahoo.com>
2024-01-25 01:36:26 +0330

    dd

--- app/Http/Controllers/Panel/ProductController.php
+++ app/Http/Controllers/Panel/ProductController.php
@@ -42,7 +42,7 @@ class ProductController extends Controller
         $support = $supportRepo->getMultiId($request->support_id);
         $this->productRepo->create_two($request , $category , $product);
         $product->supports()->sync($support);
-        return response()->json(['id', $product->id ], 200);
+        return response()->json(['id'=> $product->id ], 200);
     }
 
     public function store_three(Request $request , $product )
@@ -51,7 +51,7 @@ class ProductController extends Controller
         $multi_image = $request->multi_image ? File::image($request->file('multi_image')) : null;
         $multi_image_en = $request->multi_image_en ? File::image_en($request->file('multi_image_en')) : null;
         $this->productRepo->create_three($request ,  $product , $multi_image , $multi_image_en);
-        return response()->json(['id', $product->id ], 200);
+        return response()->json(['id'=> $product->id ], 200);
     }
     public function store_four(Request $request , $product )
     {
@@ -59,9 +59,10 @@ class ProductController extends Controller
         $video_url = $request->video_url ? File::video_peo($request->file('video_url')) : null;
         $video_url_en = $request->video_url_en ? File::video_peo_en($request->file('video_url_en')) : null;
         $this->productRepo->create_four( $product , $video_url , $video_url_en );
-        return response()->json(['id', $product->id ], 200);
+        return response()->json(['id'=> $product->id ], 200);
     }
 
+    
 
     public function store(Request $request, categoryRepo $categoryRepo, supportRepo $supportRepo)
     {
