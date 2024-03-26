b9e60c0ccddeb040f374d3e3a1f0c2add093c963
amirrezaraadi <amirrezaraadi@yahoo.com>
2024-03-26 16:00:06 +0330

    dockerssss

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,7 +3,6 @@
 if [ ! -f "vendor/autoload.php" ]; then
     composer install --no-progress --no-interaction
 fi
-
 if [ ! -f ".env" ]; then
     echo "Creating env file for env $APP_ENV"
     cp .env.example .env
